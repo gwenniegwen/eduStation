55b03e7f8ffa12d9bf008d10cd3f4e121385b44c
Kevin Hall <kevinmhall8@gmail.com>
2020-06-11 18:56:00 -0700

    styling updates

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,4 +1,5 @@
 import React from "react";
+import BusGif from '../components/Welcome/schoolbus.gif'
 
 
 
@@ -6,9 +7,20 @@ function Welcome(){
 
 
     return (
-        <div className="welcome">
-            <h1>Project Name</h1>
-            <h2>Slogan Goes Here</h2>
+        <div className="welcome-container">
+        <div className="container welcome-info">
+            <div className="welcome-spacing-div" />
+            <h1 className="main-title">EduStation</h1>
+            <img 
+            src={BusGif} alt="pic" className="busGif" />
+            <h4 className="sub-title">Let's Learn Together!</h4>
+            <div className="row justify-content-md-center">
+            <div className="column welcome-button-column">
+            <button type="button" class="btn btn-outline-light sign-up-btn welcome-button">Sign Up</button>
+            <button type="button" class="btn btn-outline-light login-btn welcome-button">Log In</button>
+            </div>
+            </div>
+        </div>
         </div>
     )
 }
