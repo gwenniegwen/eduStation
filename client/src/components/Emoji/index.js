e901794678096613064cde3246e1e9f893e05668
name <email>
2020-06-11 20:21:53 -0700

    add Emoji

---
+++ client/src/components/Emoji/index.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Emoji, Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
+
+const emojiTypeList = [
+  "apple",
+  "google",
+  "twitter",
+  "emojione",
+  "messenger",
+  "facebook",
+];
+
+function App() {
+  const [emojiList, setEmojiList] = useState([]);
+  const [emojiType, setEmojiType] = useState(null);
+
+  const onClickButton = (e) => {
+    setEmojiType(e.target.name);
+  };
+
+  const onSelect = (emoji) => {
+    console.log({ emoji });
+    setEmojiList([...emojiList, emoji]);
+    setEmojiType(null);
+  };
+  return (
+    <>
+      <p>
+        {emojiTypeList.map((name) => (
+          <button onClick={onClickButton} name={name} key={name}>
+            {name}
+          </button>
+        ))}
+      </p>
+      {emojiType && (
+        <Picker
+          onSelect={(emoji) => onSelect({ ...emoji, emojiType })}
+          set={emojiType}
+          i18n={{
+            search: "Search",
+            categories: {
+              search: "Result",
+            },
+          }}
+          style={{
+            position: "absolute",
+            zIndex: "1",
+          }}
+        />
+      )}
+      {emojiList.length
+        ? emojiList.map(({ id, emojiType }, i) => (
+            <Emoji
+              emoji={id}
+              size={32}
+              set={emojiType}
+              onClick={(emoji) => onSelect({ ...emoji, emojiType })}
+              key={i}
+            />
+          ))
+        : null}
+    </>
+  );
+}
+
+export default App;
--- client/src/pages/Performance/index.js
+++ client/src/pages/Performance/index.js
@@ -1,30 +1,3 @@
-// import React from "react";
-// import Comment from "../components/Todo/CommentForm";
-
-// class Performance extends React.Component {
-
-//   render() {
-
-//     return (
-//       <div className='main-wrapper'>
-//         <div className='main'>
-//           <div className='copy-container'>
-//             <h1>Performance.</h1>
-//             <h2>From your teacher!</h2>
-//           </div>
-
-//           <div className='contact-container'>
-//             <h3>Comment</h3>
-//             <Comment />
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Performance;
-
 import React, { useReducer, useRef } from "react";
 // import Emoji from "react-emoji-render";
 
