2a9e1fd9a33dcde721cadb77abf361f07c0c7c17
name <email>
2020-06-19 16:01:36 -0700

    Revert "Create data for Emoji"
    This reverts commit eb6b53ed302073a897577761e489cafa3df056af.

--- client/src/components/Emoji/Emoji.js
+++ client/src/components/Emoji/Emoji.js
@@ -1,62 +1,20 @@
 import React, { useState } from "react";
 import { Emoji, Picker } from "emoji-mart";
 import "emoji-mart/css/emoji-mart.css";
-// BRANDON ADDED IMPORT
-import "../../pages/Performance/index"
 
-
-const mockData = [
-  {
-    day: "Monday",
-    emojis: [
-      {
-        emoji: {
-          colons: ":+1:",
-          emojiType: "apple",
-          emoticons: [],
-          id: "+1",
-          name: "Thumbs Up Sign",
-          native: "👍",
-          short_names: ["+1", "thumbsup"],
-          skin: 1,
-          unified: "1f44d",
-        },
-      },
-      {
-        emoji: {
-          colons: ":trophy:",
-          emojiType: "apple",
-          emoticons: [],
-          id: "trophy",
-          name: "Trophy",
-          native: "🏆",
-          short_names: ["trophy"],
-          skin: null,
-          unified: "1f3c6",
-        },
-      },
-    ],
-  },
-];
 const emojiTypeList = ["apple"];
 
-function AddEmoji(props) {
-  const [emojiList, setEmojiList] = useState([mockData[0].emojis]);
+function AddEmoji() {
+  const [emojiList, setEmojiList] = useState([]);
   const [emojiType, setEmojiType] = useState(null);
-  console.log(emojiType);
-  console.log(emojiList);
+
   const onClickButton = (e) => {
-    // BRANDON ADDED IF STATEMENT
-
-      console.log(e.target.name);
-      setEmojiType(e.target.name);
-
-    
+    console.log(e.target.name);
+    setEmojiType(e.target.name);
   };
 
   const onSelect = (emoji) => {
-
-    console.log("a",{ emoji });
+    console.log({ emoji });
     setEmojiList([...emojiList, emoji]);
     setEmojiType(null);
   };
@@ -100,7 +58,7 @@ function AddEmoji(props) {
               emoji={id}
               size={32}
               set={emojiType}
-              onClick={(emoji) =>  ({ ...emoji, emojiType })}
+              onClick={(emoji) => onSelect({ ...emoji, emojiType })}
               key={i}
             />
           ))
