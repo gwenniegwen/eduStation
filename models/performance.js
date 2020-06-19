ec5716cf1a839c05eea603984b6b8779f830bbe3
name <email>
2020-06-19 16:36:13 -0700

    Create data files

--- models/performance.js
+++ models/performance.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const commentSchema = new Schema({
+const performanceSchema = new Schema({
   user: { type: String, required: true },
   content: { type: String, required: true },
   postID: { type: String, required:true},
   date: { type: Date, default: Date.now }
 });
 
-const Comment = mongoose.model("Comment", commentSchema);
+const Performance = mongoose.model("Performance", performanceSchema);
 
-module.exports = Comment;
+module.exports = Performance;
