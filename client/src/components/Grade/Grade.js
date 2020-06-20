8a58150d3382743f55287cf62c0e6a0d1da173b5
name <email>
2020-06-20 09:44:53 -0700

    comment out Grade component

--- client/src/components/Grade/Grade.js
+++ client/src/components/Grade/Grade.js
@@ -1,34 +1,34 @@
-import React from "react";
-function Grade() {
-  const [state, setState] = React.useState({
-    monday: "",
-    tuesday: "",
-    wednesday: "",
-    thursday: "",
-    friday: ""
-  })
-  return (
-    <form>
-      <label>
-        Grade
-        <input
-          type="text"
-          name="monday"
-          value={state.monday}
-          onClick={handleSubmit}
-        />
-      </label>
-      <label>
-        Grade
-        <input
-          type="text"
-          name="tuesday"
-          value={state.tuesday}
-          onClick={handleSubmit}
-        />
-      </label>
-    </form>
-  );
-}
+// import React from "react";
+// function Grade() {
+//   const [state, setState] = React.useState({
+//     monday: "",
+//     tuesday: "",
+//     wednesday: "",
+//     thursday: "",
+//     friday: ""
+//   })
+//   return (
+//     <form>
+//       <label>
+//         Grade
+//         <input
+//           type="text"
+//           name="monday"
+//           value={state.monday}
+//           onClick={handleSubmit}
+//         />
+//       </label>
+//       <label>
+//         Grade
+//         <input
+//           type="text"
+//           name="tuesday"
+//           value={state.tuesday}
+//           onClick={handleSubmit}
+//         />
+//       </label>
+//     </form>
+//   );
+// }
 
-export default Grade;
+// export default Grade;
